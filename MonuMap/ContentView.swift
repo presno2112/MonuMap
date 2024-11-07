ad1175d7ae05d73237eca9a1ee84923945bdb3c2
Sebastian Presno <sebastiandash@icloud.com>
2024-11-07 15:33:35 +0100

    Initial Commit

---
+++ MonuMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MonuMap
+//
+//  Created by Sebastian Presno on 07/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MonuMap/MonuMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonuMapApp.swift
+//  MonuMap
+//
+//  Created by Sebastian Presno on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MonuMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
